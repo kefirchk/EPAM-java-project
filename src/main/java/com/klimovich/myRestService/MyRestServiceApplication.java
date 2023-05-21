0d31d14170cd3626bb0cfa37db60d166ce5e7f97
Alexei Klimovich <90150234+kefirchk@users.noreply.github.com>
2023-05-21 15:23:37 +0300

    Add files via upload

---
+++ src/main/java/com/klimovich/myRestService/MyRestServiceApplication.java
@@ -0,0 +1,15 @@
+package com.klimovich.myRestService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class MyRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRestServiceApplication.class, args);
+	}
+
+}
